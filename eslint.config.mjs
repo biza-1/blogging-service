58eac437b45871f918deeba95bf3042c2387f7fa
biza-1 <matejbiza@gmail.com>
2024-12-07 20:45:12 +0100

    chore(eslint): ignore unused variables in rest siblings and arguments starting with '_'

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,21 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
     { files: ['**/*.{js,mjs,cjs,ts}'] },
     { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
+    {
+        rules: {
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    'argsIgnorePattern': '^_', // To ignore unused arguments that start with '_'
+                    'ignoreRestSiblings': true, // To ignore unused variables in the rest of the destructured variables
+                }
+            ],
+        }
+    }
 ];
